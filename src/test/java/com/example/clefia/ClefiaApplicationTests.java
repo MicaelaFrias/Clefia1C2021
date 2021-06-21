14d45fff91d860c52b670e9a27343af6cd9e95f1
NicolasSap <nicolas.sapaya@gmail.com>
2021-06-21 13:40:27 -0300

    Adding project

---
+++ src/test/java/com/example/clefia/ClefiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clefia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClefiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
